6033ffa4bfea99637a1a520d2f9f0208b5c93402
jeonghun <jhe.kim@samsung.com>
2024-07-11 11:02:31 +0900

    [unittest] step 7

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -6,7 +6,6 @@ from booking_scheduler import BookingScheduler
 from communication_test import TestableSmsSender
 from schedule import Customer, Schedule
 
-
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
 CUSTOMER = Customer("Fake name", "010-1234-5678")
@@ -14,10 +13,13 @@ CUSTOMER = Customer("Fake name", "010-1234-5678")
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
+
 class BookingSchedulerTest(unittest.TestCase):
 
     def setUp(self):
         self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+        self.testable_sms_sender = TestableSmsSender()
+        self.booking_scheduler.set_sms_sender(self.testable_sms_sender)
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         # arrange
@@ -65,16 +67,13 @@ class BookingSchedulerTest(unittest.TestCase):
 
     def test_예약완료시_SMS는_무조건_발송(self):
         # arrange
-        testable_sms_sender = TestableSmsSender()
         schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        self.booking_scheduler.set_sms_sender(testable_sms_sender)
 
         # act
         self.booking_scheduler.add_schedule(schedule)
 
         # assert
-        self.assertTrue(testable_sms_sender.is_send_method_is_called())
-
+        self.assertTrue(self.testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
